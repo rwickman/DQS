9324338e0e3ff2968637c46ee9d2a0ef455840db
Ryan Wickman <rwickman@memphis.edu>
2023-11-10 20:29:51 -0600

    Update plotting.

--- neat_rl/helpers/plot_exp_results.py
+++ neat_rl/helpers/plot_exp_results.py
@@ -41,15 +41,15 @@ mpl.rcParams['ps.fonttype'] = 42
 
 #rc('font', weight='bold')
 benchmark_results_dirs = [
-    "results_PGA/results_PGA-MAP-Elites",
-    "results_PGA/results_MAP-Elites-ES",
-    "results_PGA/results_TD3",
-    "results_PGA/results_CMA-MAP-Elites",
-    "results_PGA/results_QDRL"
+    "../NEAT_RL/results_PGA/results_PGA-MAP-Elites",
+    "../NEAT_RL/results_PGA/results_MAP-Elites-ES",
+    "../NEAT_RL/results_PGA/results_TD3",
+    "../NEAT_RL/results_PGA/results_CMA-MAP-Elites",
+    "../NEAT_RL/results_PGA/results_QDRL"
 ]
 rcParams['text.latex.preamble'] = [r'\usepackage{sfmath} \boldmath']
 
-my_results = "all_trained_models/walker"
+my_results = "models/walker"
 # env = "Walker"
 eval_nums = [20000, 40000, 60000, 80000, 100000]
 x_ticks = [0.2, 0.4, 0.6, 0.8, 1.0]
@@ -59,6 +59,7 @@ def get_my_stats(my_results):
     my_results_dirs = [os.path.join(my_results, d) for d in os.listdir(my_results)]
     fitness_vals = [[] for _ in range(len(eval_nums))]
     qd_vals = [[] for _ in range(len(eval_nums))]
+    coverage_vals = [[] for _ in range(len(eval_nums))]
 
     for d in my_results_dirs:
         # Open up the train JSON file 
@@ -75,6 +76,7 @@ def get_my_stats(my_results):
 
             fitness_vals[i].append(train_dict["max_fitness"][cur_idx])
             qd_vals[i].append(train_dict["total_fitness_archive"][cur_idx])
+            coverage_vals[i].append(train_dict["coverage"][cur_idx])
     
     if "walker" in my_results.lower():
         print(f"Walker MAX FITNESS {max(fitness_vals[i])} AVG FITNESS {sum(fitness_vals[i])/len(fitness_vals[i])} AVG QD SCORE {sum(qd_vals[i])/len(qd_vals[i])}" )
@@ -84,14 +86,17 @@ def get_my_stats(my_results):
         print(f"HOPPER MAX FITNESS {max(fitness_vals[i])} AVG FITNESS {sum(fitness_vals[i])/len(fitness_vals[i])} AVG QD SCORE {sum(qd_vals[i])/len(qd_vals[i])}" )
     else:
         print(f"ANT MAX FITNESS {max(fitness_vals[i])} AVG FITNESS {sum(fitness_vals[i])/len(fitness_vals[i])} AVG QD SCORE {sum(qd_vals[i])/len(qd_vals[i])}" )
-    return fitness_vals, qd_vals  
+    return fitness_vals, qd_vals, coverage_vals 
 
 def get_their_stats(root_dir, eval_num):
+    """Get the metrics from other methods."""
     results_dirs = os.listdir(root_dir)
 
     fitness_vals = [[] for _ in range(eval_num)]
     qd_vals = [[] for _ in range(eval_num)]
     min_fitness = [[] for _ in range(eval_num)]
+    coverage_vals = [[] for _ in range(eval_num)]
+
     for path_dir in results_dirs[:10]:
         if ".DS_Store" in path_dir:
             continue
@@ -106,8 +111,9 @@ def get_their_stats(root_dir, eval_num):
             fitness_vals[i].append(df["max_fitness"][i])
             qd_vals[i].append(df["sum_fitness"][i])
             min_fitness[i].append(df["min_fitness"][i])
+            coverage_vals[i].append(df["coverage"][i])
 
-    return fitness_vals, qd_vals
+    return fitness_vals, qd_vals, coverage_vals
 
 
 def shade_bounds(mean_vals, std_vals, ax, x_vals):
@@ -137,11 +143,7 @@ def construct_df(vals, x_key, y_key, x_vals):
     d[x_key] = np.array(d[x_key])
     return pd.DataFrame(d)
 
-def plot(max_fitness, qd_scores, axs, ax_idx):
-    # axs[0].set(ylabel="Max-fitness")
-    # axs[1].set(ylabel="QD-score")
-
-    #x_vals = [20000, 40000, 60000, 80000, 100000]
+def plot(max_fitness, qd_scores, coverage, axs, ax_idx):
     x_vals = [0.2, 0.4, 0.6, 0.8, 1.0]
     
     line_plots = []
@@ -149,15 +151,15 @@ def plot(max_fitness, qd_scores, axs, ax_idx):
     for i in range(len(max_fitness)):
         max_fitness_df = construct_df(max_fitness[i], "Evaluation", "Max-Fitness", x_vals)
         qd_score_df = construct_df(qd_scores[i], "Evaluation", "QD-Score", x_vals)
+        coverage_df = construct_df(coverage[i], "Evaluation", "Coverage", x_vals)
+
 
         sns.lineplot(ax=axs[0][ax_idx], data=max_fitness_df, x="Evaluation", y="Max-Fitness", ci="sd")
         
         axes_subplot = sns.lineplot(ax=axs[1][ax_idx], data=qd_score_df, x="Evaluation", y="QD-Score", ci="sd")
-        line_plots.append([r for r in axs[1][ax_idx].get_children() if type(r)==Line2D][-1])
-        
-        # Show the standard deviation shaded region
-        #shade_bounds(max_fitness[i], fitness_bounds[i], axs[0][ax_idx], x_vals)
-        # shade_bounds(qd_scores[i], qd_bounds[i], axs[1][ax_idx], x_vals)
+        axes_subplot = sns.lineplot(ax=axs[2][ax_idx], data=coverage_df, x="Evaluation", y="Coverage", ci="sd")
+
+        line_plots.append([r for r in axs[2][ax_idx].get_children() if type(r)==Line2D][-1])
 
     return line_plots
 
@@ -165,40 +167,45 @@ if __name__ == "__main__":
     envs = ["Walker", "HalfCheetah", "Ant", "Hopper"]
     labels = ["DQS (ours)", "PGA-MAP-Elites", "MAP-Elites-ES", "TD3", "CMA-MAP-Elites", "QD-PG"]
 
-    fig, axs = plt.subplots(2, 4, figsize=(20, 7))
+    fig, axs = plt.subplots(3, 4, figsize=(20, 11))
     for i, env in enumerate(envs):
         max_fitness_list = []
         qd_score_list = []
+        coverage_list = []
 
         # Plot DQS results
-        my_results = f"all_trained_models/{env.lower()}"
-        fitness, qd_scores = get_my_stats(my_results)
+        my_results = f"models/{env.lower()}"
+        fitness, qd_scores, coverage = get_my_stats(my_results)
         max_fitness_list.append(fitness)
         qd_score_list.append(qd_scores)
+        coverage_list.append(coverage)
 
         # Plot the benchmarks
         for d in benchmark_results_dirs:
             method = d.split("_")[-1]
                 
             d = os.path.join(d, f"results_{env}_{method}")
-            max_fitness, qd_score = get_their_stats(d, 5)
+            max_fitness, qd_score, coverage = get_their_stats(d, 5)
             
             max_fitness_list.append(max_fitness)
             qd_score_list.append(qd_score)
+            coverage_list.append(coverage)
 
 
-        cur_line_plots = plot(max_fitness_list, qd_score_list, axs, i)
+        cur_line_plots = plot(max_fitness_list, qd_score_list, coverage_list, axs, i)
         
         if "Hopper" in env:
             line_plots = cur_line_plots
 
         axs[0][i].set_title("QD" + env, fontsize=24)
-        axs[1][i].set_xlabel("Evaluations (x$10^5$)", fontsize=20)
+        axs[2][i].set_xlabel("Evaluations (x$10^5$)", fontsize=20)
         axs[0][i].set_xlabel(None)
+        axs[1][i].set_xlabel(None)
         axs[0][i].tick_params(labelbottom=False, labelsize=12)
-        axs[1][i].tick_params(labelsize=12)
+        axs[1][i].tick_params(labelbottom=False, labelsize=12)
+        axs[2][i].tick_params(labelsize=12)
 
-        for j in range(2):
+        for j in range(3):
             axs[j, i].spines['top'].set_visible(False)
             axs[j, i].spines['right'].set_visible(False)
             axs[j, i].spines['left'].set_visible(False)
@@ -206,18 +213,23 @@ if __name__ == "__main__":
 
         axs[0][i].set_xticks(x_ticks)
         axs[1][i].set_xticks(x_ticks)
+        axs[2][i].set_xticks(x_ticks)
         
         axs[0][i].set_ylabel(None)
         axs[1][i].set_ylabel(None)
+        axs[2][i].set_ylabel(None)
 
         axs[0][i].yaxis.set_major_formatter(CustomFormatter(useMathText=True))
         axs[1][i].yaxis.set_major_formatter(CustomFormatter(useMathText=True))
-        axs[1][i].xaxis.set_major_formatter(CustomFormatter(useMathText=True))
-        axs[1][i].ticklabel_format(axis='y', style='sci', scilimits=(2,4))
+        axs[2][i].yaxis.set_major_formatter(CustomFormatter(useMathText=True))
+
+        axs[2][i].xaxis.set_major_formatter(CustomFormatter(useMathText=True))
+        axs[2][i].ticklabel_format(axis='y', style='sci', scilimits=(2,4))
 
     
     axs[0][0].set_ylabel("Max-Fitness", fontsize=20)
     axs[1][0].set_ylabel("QD-Score", fontsize=20)
+    axs[2][0].set_ylabel("Coverage", fontsize=20)
 
     lgd = fig.legend(handles=line_plots, labels=labels, loc="lower center", ncol=len(labels), fontsize=18, frameon=False)
     
